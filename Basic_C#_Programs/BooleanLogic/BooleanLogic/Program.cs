45440bf7db3ab9c44c5054150a1dd67d541b009d
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-01 15:27:30 -0800

    quick update

--- Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -10,7 +10,7 @@ namespace BooleanLogic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(true && true && true || false);
+            //Console.WriteLine(true && true && true || false);
             //Console.WriteLine(true && false);
             //Console.WriteLine(true && true);
             //Console.WriteLine(false && false);
@@ -32,6 +32,54 @@ namespace BooleanLogic
             //Console.WriteLine(false ^ false);
 
 
+
+            //using if, else if, and else 
+
+            //string name = "Kai";
+
+            //if (name == "Kai")
+            //{
+            //    Console.WriteLine("Your name is Kai");
+            //}
+            //else if (name == "Rowan")
+            //{
+            //    Console.WriteLine("Your name is Rowan");
+            //}
+            //else if (name == "Pheobe")
+            //{
+            //    Console.WriteLine("Your name is Pheobe");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not recognized");
+            //}
+
+            //string name = "Kai";
+            //if (name != "Jesse")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+            
+
+            int age = 25;
+            if (age > 20)
+            {
+                Console.WriteLine("Here is your wine!"); 
+            }
+            else
+            {
+                Console.WriteLine("No you may not have that wine."); 
+            }
+
+            
+            if (age <= 20)
+            {
+                Console.WriteLine("No you may not have that wine.");
+            }
+            else
+            {
+                Console.WriteLine("Here is your wine!");
+            }
 
             Console.ReadLine();
         }
---
+++ Basic_C#_Programs/IfElseStatements/IfElseStatements/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using if, else if, and else 
+
+            //string name = "Kai";
+
+            //if (name == "Kai")
+            //{
+            //    Console.WriteLine("Your name is Kai");
+            //}
+            //else if (name == "Rowan")
+            //{
+            //    Console.WriteLine("Your name is Rowan");
+            //}
+            //else if (name == "Pheobe")
+            //{
+            //    Console.WriteLine("Your name is Pheobe");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not recognized");
+            //}
+
+            //string name = "Kai";
+            //if (name != "Jesse")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+
+
+            //int age = 25;
+            //if (age > 20)
+            //{
+            //    Console.WriteLine("Here is your wine!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No you may not have that wine.");
+            //}
+
+
+            //if (age <= 20)
+            //{
+            //    Console.WriteLine("No you may not have that wine.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Here is your wine!");
+            //}
+
+            int num1 = 7;
+            int num2 = 12;
+
+            string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
