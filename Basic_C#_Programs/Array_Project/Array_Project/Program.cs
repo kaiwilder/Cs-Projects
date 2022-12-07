d65f640ff799232929717d650e6cc16ab104fccf
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-07 12:35:25 -0800

    Array Project

--- Basic_C#_Programs/Array_Project/Array_Project/Program.cs
+++ Basic_C#_Programs/Array_Project/Array_Project/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Array_Project
 {
@@ -12,17 +10,89 @@ namespace Array_Project
         {
             string[] stringArray1 = { "apple", "orange", "pear", "banana" };
             Console.WriteLine("Select an index of the array (0-3)");
-            
-            stringArray1[i] = Console.ReadLine();
+            int string1 = Convert.ToInt32(Console.ReadLine());
 
-            int[] intArray1 = { 3, 7, 15, 26, 1 };
-            Console.WriteLine("Select an index of the array (0-4)");
+            
+            if(string1 == 0)
+            {
+                Console.WriteLine(stringArray1[0]);
+            }
+            else if (string1 == 1)
+            {
+                Console.WriteLine(stringArray1[1]);
+            }
+            else if (string1 == 2)
+            {
+                Console.WriteLine(stringArray1[2]);
+            }
+            else if (string1 == 3)
+            {
+                Console.WriteLine(stringArray1[3]);
+            }
+            else
+            {
+                Console.WriteLine("Please pick a number between 0 and 3");
+            }
+
+            
+
+
+
+            int[] intArray1 = { 3, 7, 15, 26, };
+            Console.WriteLine("Select an index of the array (0-3)");
+            int nums = Convert.ToInt32(Console.ReadLine());
+
+            if (nums == 0)
+            {
+                Console.WriteLine(intArray1[0]);
+            }
+            if (nums == 1)
+            {
+                Console.WriteLine(intArray1[1]);
+            }
+            if (nums == 2)
+            {
+                Console.WriteLine(intArray1[2]);
+            }
+            if (nums == 3)
+            {
+                Console.WriteLine(intArray1[3]);
+            }
+            else
+            {
+                Console.WriteLine("Please pick a number between 0 and 3");
+            }
+            
 
             List<string> stringList = new List<string>();
             stringList.Add("apple");
             stringList.Add("orange");
             stringList.Add("pear");
             stringList.Add("banana");
+            Console.WriteLine("Select an index for the list: ");
+            int list1 = Convert.ToInt32(Console.ReadLine());
+
+            if (list1 == 0)
+            {
+                Console.WriteLine(stringList[0]);
+            }
+            if (list1 == 1)
+            {
+                Console.WriteLine(stringList[1]);
+            }
+            if (list1 == 2)
+            {
+                Console.WriteLine(stringList[2]);
+            }
+            if (list1 == 3)
+            {
+                Console.WriteLine(stringList[3]);
+            }
+            else
+            {
+                Console.WriteLine("Please pick a number between 0 and 3");
+            }
+            Console.ReadLine();
         }
     }
 }
