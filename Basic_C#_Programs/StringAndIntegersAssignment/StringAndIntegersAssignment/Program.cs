daaaf9d8ed7fa9dac292f80599a258b75e3ef8eb
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-08 16:12:33 -0800

    Strings and Integers Assignment

--- Basic_C#_Programs/StringAndIntegersAssignment/StringAndIntegersAssignment/Program.cs
+++ Basic_C#_Programs/StringAndIntegersAssignment/StringAndIntegersAssignment/Program.cs
@@ -12,28 +12,32 @@ namespace StringAndIntegersAssignment
         {
             try
             {
-                List<int> Numbers = new List<int>() { 5, 10, 15, 20, 25 };
+                List<int> Numbers = new List<int>() { 5, 10, 15, 20, 25 };   //instantiated list
                 Console.WriteLine("Pick a number to divide by");
-                int divisor = Convert.ToInt32(Console.ReadLine());
+                int divisor = Convert.ToInt32(Console.ReadLine());           //saves user input
 
                 foreach (int number in Numbers)
                 {
-                    int quotient = number / divisor;
+                    int quotient = number / divisor;                         //divides each number byt the user input
                     Console.WriteLine(quotient);
                 }
                 Console.ReadLine();
             }
-            catch (DivideByZeroException)
+            catch (DivideByZeroException)                                    
             {
-                Console.WriteLine("Please do not divide by zero");
+                Console.WriteLine("Please do not divide by zero");           //if 0 is input, prints  this line
             }
             catch (FormatException)
             {
-                Console.WriteLine("Please enter a whole number");
+                Console.WriteLine("Please enter a whole number");            //if not a number is input, prints this line
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                               //if any other exception occurs, prints the systems error message 
             }
             finally
             {
-                Console.WriteLine("Continuing with calculations..");
+                Console.WriteLine("Program Complete");                        //after the program has run entirely prints this line
                 Console.ReadLine();
             }
         }
