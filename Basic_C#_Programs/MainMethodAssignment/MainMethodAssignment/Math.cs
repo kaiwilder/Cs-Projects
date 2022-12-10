dd2719a81afc0893a9aeebcdb4b8e19e6b3ee8d9
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-10 12:20:26 -0800

    Main Method Submission Assignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Math
+    {
+        //This method takes in an integer and adds 5, returns the total
+        public int One(int x)  
+        {
+            return 5 + x;
+        }
+        //takes in a decimal, converts total to an integer
+        public decimal One(decimal y)
+        {
+            int z = Decimal.ToInt32(y);
+            return 8 + z;
+        }
+        //supposed to take in a string, convert it to an integer to do math to it
+        public string One(string w)
+        {
+            int a = Convert.ToInt32(w);
+            string b = Convert.ToString(2 * a);
+            return b;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the class math
+            Math MyMethod = new Math();
+
+            //taking user input, converting type string to integer
+            Console.WriteLine("Pick a number: ");
+            int x = Convert.ToInt32(Console.ReadLine()); 
+
+            //calls method One
+            Console.WriteLine(MyMethod.One(x));
+
+            //asks user for a decimal, converts type string to decimal
+            Console.WriteLine("Pick a decimal number: ");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            //calls the other method One, takes in a decimal, outputs an integer
+            Console.WriteLine(MyMethod.One(y));
+
+            //asks user for a number but it;s actually a string
+            Console.WriteLine("Pick a new number: ");
+            string w = Console.ReadLine();
+
+            //this One takes a string, converts to an integer to do some math,
+            //converts back to a string
+            Console.WriteLine(MyMethod.One(w));
+            Console.ReadLine();
+        }
+    }
+}
