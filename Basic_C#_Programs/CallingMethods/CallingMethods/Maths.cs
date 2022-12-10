9f7ec639fb2a0d6a025f2f9d77aaabbe0e958726
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-09 16:14:15 -0800

    Calling Methods Submission Assignment

---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/Maths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Maths
+    {
+        public int Addition(int x)        //creating a method that adds 5 to the user input number
+        {
+            return 5 + x;
+        }
+        public int Multiply(int x)        //creating a method that multiplies 5 by the user input number
+        {
+            return 5 * x;
+        }
+        public int Divide(int x)          //creating a method that divides the user input number by 2
+        {
+            return x / 2;
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Pick a number: ");
+            int x = Convert.ToInt32(Console.ReadLine()); //stores the user input
+
+            Maths MyMethod = new Maths();               //creates an object within maths class
+
+            Console.WriteLine(MyMethod.Addition(x));    //calls the addition method
+            Console.WriteLine(MyMethod.Multiply(x));    //calls the multiply method
+            Console.WriteLine(MyMethod.Divide(x));      //calls the divide method
+            Console.ReadLine();
+            }
+    }
+}
