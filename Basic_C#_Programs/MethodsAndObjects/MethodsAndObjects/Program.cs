362a41b852d89a9be448f7065e0f4a29882c191a
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-12 15:51:46 -0800

    MethodsAndObjects Submission

---
+++ Basic_C#_Programs/MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Employee object called Name
+            Employee Name = new Employee();
+            //initializing Employee Object
+            Name.FirstName = "Sample";
+            Name.LastName = "Student";
+            //using the superclass method SayName()
+            Name.SayName();
+            Console.ReadLine();
+        }
+    }
+}
