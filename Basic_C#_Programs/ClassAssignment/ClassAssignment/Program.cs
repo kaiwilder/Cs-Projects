0b0b4dd26d4b914b8231e9dd7f7f580158f57fc9
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-12 12:20:01 -0800

    Resubmission Main Method assignment

---
+++ Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        public static void Sum(out int G)
+        {
+            G = 80;
+            G += G;
+        }
+        static void Main(string[] args)
+        {
+            int g;
+            Sum (out g);
+            Console.WriteLine("The sum of" +
+                " the value is: {0}", g);
+            Console.ReadLine();
+        }
+    }
+}
