f09b75cc815a9db9084cf14e37a467bc72415b67
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-16 17:36:48 -0800

    Method Submission assignment

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,39 +10,22 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //Game game = new TwentyOneGame(); //polymorphism
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Jesse", "Jill", "Bob" };
-            //game.ListPlayers();
-            //Console.ReadLine();
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-            //game.ListPlayers();
-            //Console.ReadLine();
-
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Kai";
+            game += player;  //equal to game = game + player
+            game -= player; //equal to game = game - player;
+            
             Deck deck = new Deck();
             deck.Shuffle(3);
-
-            //int timesShuffled = 0;
-            //deck = Shuffle(deck, out timesShuffled, times: 3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            //Console.WriteLine("Times shuffled: {0} ", timesShuffled);
             Console.ReadLine();
         }
-
-        
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++) 
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
