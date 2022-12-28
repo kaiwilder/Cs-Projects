e5b5b786f9384b9fc93fe53ee088788b09e378cb
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2022-12-27 17:18:06 -0800

    Constructor Assignment

--- Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Constructor.cs
+++ Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Constructor.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;     
 
 namespace ConstructorAssignment
 {
-    class Constructor
+    public class Constructor
     {
+        public string name { get; set; }
+        public int num { get; set; }
+
+        public Constructor() : this("Tim", 10)
+        {
+        }
+        public Constructor(string name, int num)
+        {
+            this.name = name;
+            this.num = num;
+        }
     }
-}
+}                               
--- Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Program.cs
+++ Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -10,12 +10,15 @@ namespace ConstructorAssignment
     {
         static void Main(string[] args)
         {
-            const string filler = "this is my constant variable";
+            const string filler = "this is my constant variable.";
 
             var sentence = "This is my var variable";
 
             Console.WriteLine("{0} and {1}", sentence, filler);
-            Console.Read();
+
+            Constructor obj = new Constructor();
+            Console.WriteLine(obj.name);
+            Console.ReadLine();
         }
     }
 }
