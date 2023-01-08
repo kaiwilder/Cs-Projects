7eb3435aa02afd7daebaecf3048f154c64312f14
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2023-01-07 17:39:45 -0800

    CarInsurance Project

---
+++ Basic_C#_Programs/MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Random rnd = new Random(10);
+            //int num = rnd.Next();
+
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\kaiwi\OneDrive\Documents\GitHub\Cs-Projects\Basic_C#_Programs\log.txt", text);
+
+            //List<string> names = new List<string>
+            //{
+            //    "Jesse",
+            //    "Adam",
+            //    "Brett"
+            //};
+
+            //int number = 5;
+
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Jesse";
+            user.LastName = "Johnson";
+            user.Age = 32;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            throw new Exception("Invalid page");
+
+            return View();
+        }
+
+        public ActionResult Contact(int id=0)
+        {
+            ViewBag.Message = id;
+
+            return View();
+        }
+    }
+}
---
+++ Basic_C#_Programs/NewsletterAppMvc/NewsletterAppMvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using NewsletterAppMvc.Models;
+using NewsletterAppMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMvc.Controllers
+{                                                   
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (NewsletterEntitys db = new NewsletterEntitys())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                return View("Success");
+            }
+        }
+    }
+}
