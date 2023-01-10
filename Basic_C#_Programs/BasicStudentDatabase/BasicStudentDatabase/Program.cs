e939404ace004b749877623cdb816cf6994b920b
Kai Wilder <113560181+kaiwilder@users.noreply.github.com>
2023-01-10 13:55:36 -0800

    Basic Student Database project

---
+++ Basic_C#_Programs/BasicStudentDatabase/BasicStudentDatabase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Billy" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
